cf7262fb0d20ed0de102dbc0f5a77565f1c8b0ee
Patrick Thibaudeau <thibaud@yorku.ca>
2025-03-27 13:59:06 -0400

    Removed all cria settings.

--- settings.php
+++ settings.php
@@ -29,30 +29,6 @@ if ($hassiteconfig) {
     $settings = new admin_settingpage('block_design_ideas_settings', new lang_string('pluginname', 'block_design_ideas'));
 
     $settings->add(new admin_setting_configtext(
-        'block_idi_cria_url',
-        get_string('cria_url', 'block_design_ideas'),
-        get_string('cria_url_help', 'block_design_ideas'),
-        '',
-        PARAM_TEXT
-    ));
-
-    $settings->add(new admin_setting_configpasswordunmask(
-        'block_idi_cria_token',
-        get_string('cria_token', 'block_design_ideas'),
-        get_string('cria_token_help', 'block_design_ideas'),
-        0,
-        PARAM_INT
-    ));
-
-    $settings->add(new admin_setting_configtext(
-        'block_idi_cria_bot_id',
-        get_string('cria_bot_id', 'block_design_ideas'),
-        get_string('cria_bot_id_help', 'block_design_ideas'),
-        0,
-        PARAM_INT
-    ));
-
-    $settings->add(new admin_setting_configtext(
         'block_idi_semantic_scholar_api_url',
         get_string('semantic_scholar_api_url', 'block_design_ideas'),
         get_string('semantic_scholar_api_url_help', 'block_design_ideas'),
@@ -66,20 +42,6 @@ if ($hassiteconfig) {
         get_string('semantic_scholar_api_key_help', 'block_design_ideas'),
         '',
         PARAM_TEXT
-    ));
-
-    $settings->add(new admin_setting_configselect(
-        'block_idi_institution',
-        get_string('institution_type', 'block_design_ideas'),
-        get_string('institution_type_help', 'block_design_ideas'),
-        1,
-        [
-            '1' => get_string('university', 'block_design_ideas'),
-            '2' => get_string('college', 'block_design_ideas'),
-            '3' => get_string('high_school', 'block_design_ideas'),
-            '4' => get_string('elementary', 'block_design_ideas')
-        ],
-        PARAM_INT
     ));
 
     $settings->add(new admin_setting_description(
